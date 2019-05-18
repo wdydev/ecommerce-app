230f4a6cea54b4addc62edfa2cef59865de9efbd
Sandeep Giri <ioesandeep@gmail.com>
2019-05-18 16:15:21 -0500

    entities declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component, Renderer2} from '@angular/core';
+import {Component, Renderer2, ViewEncapsulation} from '@angular/core';
 import {NavigationStart, Router} from '@angular/router';
 import {setTheme} from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: `./app.component.html`,
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'mwa-app';
