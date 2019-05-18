29fa036dc8e96474698ff29a3763a3011d60f547
Karim Bayoumi <kerry688@hotmail.com>
2019-05-18 02:44:16 -0500

    Setup myaccount , Order History , Order Information

--- src/app/frontend/my-account/my-account.component.ts
+++ src/app/frontend/my-account/my-account.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-myaccount' ,
-  templateUrl: './myaccount.component.html'
+  templateUrl: './my-account.component.html'
 })
 
 export class MyAccountComponent {}
