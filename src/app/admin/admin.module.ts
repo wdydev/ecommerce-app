3808b400b09ffa4af5c7fcd8539b5c951a22c71d
Sandeep Giri <ioesandeep@gmail.com>
2019-05-18 14:20:29 -0500

    admin dashboard components setup

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,16 +1,27 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
-import {CarouselModule} from 'ngx-owl-carousel-o';
 import {routes} from './admin.routes';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {CategoryComponent} from './category/category.component';
+import {ProductsComponent} from './products/products.component';
+import {OrdersComponent} from './orders/orders.component';
+import {CouponsComponent} from './coupons/coupons.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CarouselModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [],
+  declarations: [
+    SidebarComponent,
+    DashboardComponent,
+    CategoryComponent,
+    ProductsComponent,
+    OrdersComponent,
+    CouponsComponent
+  ],
   providers: [],
 })
 export class AdminModule {
---
+++ src/app/admin/sidebar/sidebar.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-sidebar',
+  templateUrl: './sidebar.html'
+})
+export class SidebarComponent {
+  private links: {
+    dashboard: Array<string>
+    categories: Array<string>
+    products: Array<string>
+    orders: Array<string>
+    coupons: Array<string>
+    shipping?: Array<string>,
+    logout?: Array<string>,
+  };
+
+  constructor() {
+    this.links = {
+      dashboard: ['', 'admin'],
+      categories: ['', 'admin', 'categories'],
+      products: ['', 'admin', 'products'],
+      orders: ['', 'admin', 'orders'],
+      coupons: ['', 'admin', 'coupons'],
+      shipping: ['', 'admin', 'shipping'],
+      logout: ['', 'admin', 'logout'],
+    };
+  }
+
+}
