3ec675f4afe7ba7b759d3bedce36485da9fa4370
Sandeep Giri <ioesandeep@gmail.com>
2019-05-18 01:53:16 -0500

    slider fix in home page

--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -4,6 +4,42 @@ import {Component} from '@angular/core';
   selector: 'app-frontend',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  private bannerSlider: any;
+
+  constructor() {
+    this.bannerSlider = {
+      nav: true,
+      dots: false,
+      loop: false,
+      autoplay: false,
+      lazyLoad: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 5000,
+      autoplaySpeed: 5000,
+      navRewind: true,
+      navText: ['', ''],
+      margin: 30,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true,
+        },
+        767: {
+          items: 1,
+          nav: true,
+        },
+        991: {
+          items: 1,
+          nav: true,
+        },
+        1199: {
+          items: 1,
+          nav: true,
+        },
+      }
+    };
+  }
 }
+
