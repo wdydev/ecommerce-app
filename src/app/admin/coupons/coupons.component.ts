91c15bba3c8586601cfcdd51b5fd856a00b6054b
AbduEndrisM <beazsasm@gmail.com>
2019-05-18 23:41:48 -0500

    Coupon component

--- src/app/admin/coupons/coupons.component.ts
+++ src/app/admin/coupons/coupons.component.ts
@@ -1,17 +1,26 @@
 import {Component, TemplateRef} from '@angular/core';
 import {ModalService} from '../../services/modal.service';
+import {Category} from '../../entity/category';
+import {Coupon, CouponI} from '../../entity/coupon';
 
 @Component({
   selector: 'app-admin-coupons',
   templateUrl: './coupons.component.html'
 })
 export class CouponsComponent {
+
+  private  coupon: CouponI;
   constructor(private modal: ModalService) {
 
   }
   private viewCoupon(template: TemplateRef<any>): any {
     this.modal.open(template);
 
+  }
+
+  public couponForm(template: TemplateRef<any>, coupon: any = {}) {
+    this.coupon = new Coupon(coupon);
+    this.modal.open(template);
   }
 
 }
