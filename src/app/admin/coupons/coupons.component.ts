5dcea77b817181c026301f718479444102247df6
AbduEndrisM <beazsasm@gmail.com>
2019-05-19 00:44:20 -0500

    Coupons module

--- src/app/admin/coupons/coupons.component.ts
+++ src/app/admin/coupons/coupons.component.ts
@@ -2,6 +2,8 @@ import {Component, TemplateRef} from '@angular/core';
 import {ModalService} from '../../services/modal.service';
 import {Category} from '../../entity/category';
 import {Coupon, CouponI} from '../../entity/coupon';
+import {FormControl, FormGroup} from '@angular/forms';
+import {CouponService} from './coupon.service';
 
 @Component({
   selector: 'app-admin-coupons',
@@ -9,18 +11,37 @@ import {Coupon, CouponI} from '../../entity/coupon';
 })
 export class CouponsComponent {
 
-  private  coupon: CouponI;
-  constructor(private modal: ModalService) {
+  private coupon: CouponI;
+  private form: FormGroup;
+
+  constructor(private modal: ModalService, private service: CouponService) {
 
   }
+
   private viewCoupon(template: TemplateRef<any>): any {
     this.modal.open(template);
 
   }
 
-  public couponForm(template: TemplateRef<any>, coupon: any = {}) {
+  public async couponForm(template: TemplateRef<any>, coupon: any = {}) {
     this.coupon = new Coupon(coupon);
-    this.modal.open(template);
+    this.form = new FormGroup({
+      code: new FormControl(),
+      discountType: new FormControl(),
+      discountAmount: new FormControl(),
+      startDate: new FormControl(),
+      endDate: new FormControl(),
+      minimumCost: new FormControl(),
+      maximumCost: new FormControl(),
+      status: new FormControl()
+    });
+    const res = await this.modal.open(template);
+  }
+
+
+  public async saveCoupon() {
+    // console.log(this.form.value);
+    await this.service.saveCoupon(this.form.value);
   }
 
 }
