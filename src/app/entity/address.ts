22d70cc24fa53da014e3d2bd21a2d4aa2bd27bf1
Karim Bayoumi <kerry688@hotmail.com>
2019-05-19 23:48:37 -0500

    Just to upadte

--- src/app/entity/address.ts
+++ src/app/entity/address.ts
@@ -2,8 +2,9 @@ export interface Address {
   _id?: string;
   address: string;
   address2: string;
+  zipcode: number;
   city: string;
   state: string;
   country: string;
-  zipcode: number;
+
 }
