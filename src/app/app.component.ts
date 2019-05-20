6e635e8e048efd726efb286b750b217c1fd238a3
Sandeep Giri <ioesandeep@gmail.com>
2019-05-20 02:56:10 -0500

    application header fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import {Component, Renderer2, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, Renderer2, ViewEncapsulation} from '@angular/core';
 import {NavigationStart, Router} from '@angular/router';
 import {setTheme} from 'ngx-bootstrap/utils';
+import {AppService} from './services/app.service';
+import {CategoryDisplayI, CategoryI} from './entity/category';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +10,41 @@ import {setTheme} from 'ngx-bootstrap/utils';
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'mwa-app';
+export class AppComponent implements OnInit {
+  private title: string;
+  private categories: Array<CategoryDisplayI>;
 
-  constructor(private router: Router, private renderer: Renderer2) {
-    router.events.subscribe(event => {
+  constructor(private router: Router, private renderer: Renderer2, private service: AppService) {
+    // watch for change in navigation
+    this.watchNavigation();
+
+    // bootstrap setup
+    setTheme('bs3');
+
+    // set the title
+    this.title = 'eMarket';
+  }
+
+  private watchNavigation(): void {
+    this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         const currentRoute = event.url.slice(1);
         if (!currentRoute) {
-          renderer.addClass(document.body, 'common-home');
+          this.renderer.addClass(document.body, 'common-home');
           return;
         }
-        renderer.removeClass(document.body, 'common-home');
+        this.renderer.removeClass(document.body, 'common-home');
       }
     });
+  }
 
-    setTheme('bs3');
+  public async bootstrap(): Promise<void> {
+    // make categories for nav
+    // make categories for header
+    this.categories = await this.service.prepareCategories();
+  }
+
+  public ngOnInit(): void {
+    this.bootstrap();
   }
 }
