f7defc89de679c52881fcaa855ea7993ee65fc23
Sandeep Giri <ioesandeep@gmail.com>
2019-05-20 23:05:38 -0500

    user service, checkout page

--- src/app/frontend/checkout/checkout.component.ts
+++ src/app/frontend/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CartItem} from '../../entity/cart.item';
 import {CartService} from '../../services/cart.service';
+import {UserI} from '../../entity/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-checkout',
@@ -8,13 +10,15 @@ import {CartService} from '../../services/cart.service';
 })
 export class CheckoutComponent implements OnInit {
   private cart: Array<CartItem>;
+  private user: UserI;
 
-  constructor(private service: CartService) {
+  constructor(private service: CartService, private userService: UserService) {
     this.cart = this.service.getItems();
     this.service.subscribe(items => this.cart = items);
+    this.user = userService.user;
   }
 
   public ngOnInit(): void {
-
+    console.log(this.user);
   }
 }
