693f21ec80c40dec222e4ad508c53bbb2dce3d29
Sandeep Giri <ioesandeep@gmail.com>
2019-05-21 21:55:03 -0500

    production build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiEndpoint: 'http://localhost:8000/api/v1/',
+  apiEndpoint: 'http://ec2-18-222-34-118.us-east-2.compute.amazonaws.com/api/v1/',
   awsPoolId: 'us-east-2:c7b15228-9bf0-472f-913c-0978744985c5',
   awsRegion: 'us-east-2',
   awsEndpoint: 'https://mwa2019.us-east-2.amazonaws.com/',
